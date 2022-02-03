c31ad08f60625a9bddd4800a3603fb14eedf599e
Narendirakumarkumar <naren.be2009@gmail.com>
2022-02-03 14:38:49 +0530

    UserManagement commit -020322

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,7 +4,11 @@ import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
 import { Navbar } from '../components/Navbar';
 import { Home } from '../pages/Home';
 import { InvoiceDetail } from '../pages/InvoiceDetail';
-
+import { Login } from '../pages/Login';
+import { RegisterComp } from '../components/Auth/RegisterComp';
+import { UserManagement } from '../pages/UserManagement';
+import { UserGrid } from '../components/Auth/UserGrid';
+import { UserDetail } from '../components/Auth/UserDetail';
 export const Router = () => {
   const [Data, setInvNumber] = useState({});
   const queryClient = new QueryClient();
@@ -15,6 +19,9 @@ export const Router = () => {
         <Routes>
           <Route path='/' element={<Home setInvNumber={setInvNumber} />} />
           <Route path='InvoiceDetail' element={<InvoiceDetail data={Data} />} />
+          <Route path='Login' element={<Login />} />
+          <Route path='Register' element={<RegisterComp />} />
+          <Route path='UserManagement' element={<UserManagement />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
