498cfb7c34b02b2b94c1c55d373e4cac25e038ec
KarthickBarani <ras81082@gmail.com>
2022-04-07 22:47:07 +0530

    update

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -389,7 +389,7 @@ export const Form = (props: {
                         <div className="form-group">
                             <div className="d-flex flex-stack">
                                 <label htmlFor="approver" className={formLabel}>Next Approvers</label>
-                                {props.nextApprovers.filter(arr => arr.Status === 3 || arr.Status === 0) === [] ? <button className="align-self-start btn btn-icon btn-sm btn-hover-rise" data-bs-toggle="modal" data-bs-target="#level" ><span className="svg-icon svg-icon-2 svg-icon-primary">
+                                {props.nextApprovers.filter(arr => arr.Status === 3 || arr.Status === 0).length !== 0 ? <button className="align-self-start btn btn-icon btn-sm btn-hover-rise" data-bs-toggle="modal" data-bs-target="#level" ><span className="svg-icon svg-icon-2 svg-icon-primary">
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                         viewBox="0 0 24 24" fill="none">
@@ -404,13 +404,12 @@ export const Form = (props: {
                             </div>
                         </div>
                         <ul className="list-group list-group-flush hover-scroll h-100px">
-                            {props.nextApprovers.filter(arr => arr.Status === 3 || arr.Status === 0) === [] ? props.nextApprovers.filter(arr => arr.Status === 3 || arr.Status === 0).map(user => (
+                            {props.nextApprovers.filter(arr => arr.Status === 3 || arr.Status === 0).length !== 0 ? props.nextApprovers.filter(arr => arr.Status === 3 || arr.Status === 0).map(user => (
                                 <li className="list-group-item list-group-item-action fw-bold fs-6">{user.ApproverName}</li>
-                            )) : <li><h4 className="pt-3 text-center">This invoice is ready to post <span className="svg-icon svg-icon-success svg-icon-2x"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            )) : <li><h4 className="pt-4 text-center">This invoice is ready to post <span className="svg-icon svg-icon-success svg-icon-2x"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                 <path opacity="0.5" d="M12.8956 13.4982L10.7949 11.2651C10.2697 10.7068 9.38251 10.7068 8.85731 11.2651C8.37559 11.7772 8.37559 12.5757 8.85731 13.0878L12.7499 17.2257C13.1448 17.6455 13.8118 17.6455 14.2066 17.2257L21.1427 9.85252C21.6244 9.34044 21.6244 8.54191 21.1427 8.02984C20.6175 7.47154 19.7303 7.47154 19.2051 8.02984L14.061 13.4982C13.7451 13.834 13.2115 13.834 12.8956 13.4982Z" fill="black" />
                                 <path d="M7.89557 13.4982L5.79487 11.2651C5.26967 10.7068 4.38251 10.7068 3.85731 11.2651C3.37559 11.7772 3.37559 12.5757 3.85731 13.0878L7.74989 17.2257C8.14476 17.6455 8.81176 17.6455 9.20663 17.2257L16.1427 9.85252C16.6244 9.34044 16.6244 8.54191 16.1427 8.02984C15.6175 7.47154 14.7303 7.47154 14.2051 8.02984L9.06096 13.4982C8.74506 13.834 8.21146 13.834 7.89557 13.4982Z" fill="black" />
-                            </svg></span></h4></li>
-                            }
+                            </svg></span></h4></li>}
                         </ul>
                     </div>
                     <div className="col-6">
